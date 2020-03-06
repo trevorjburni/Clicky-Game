e9ee23c92754bd94eecdb1c3fa8dac01e49190b0
Trevor Burningham <tburningham@journaltech.com>
2020-03-06 14:42:11 -0700

    pseudocoded out some of the functions that need to be added.

---
+++ clicky/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
+
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends
+  };
+
+  // Code out a shuffle function
+
+  // Code out a onclick function that will change the state.friends.clicked to true
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Clicky Game</Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
