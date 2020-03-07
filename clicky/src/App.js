d560aeec0f9c82009323b5a19c7f0bddcb8bc6a2
Trevor Burningham <trevorjburni@gmail.com>
2020-03-06 17:22:58 -0700

    Working on changing up the friend cards so that they are just images

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -8,13 +8,41 @@ class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
     friends,
-    score = 0,
-    highscore = 0
+    score: 0,
+    highscore: 0
   };
 
   // Code out a shuffle function
+  shuffleFriends() {
+    this.setState({
+      friends: this.shuffle(friends)
+    })
+  }
+
+  shuffle(arr) {
+    var currentIndex = arr.length;
+    var temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = arr[currentIndex];
+      arr[currentIndex] = arr[randomIndex];
+      arr[randomIndex] = temporaryValue;
+    }
+  
+    return arr;
+  
+  };
 
   // Code out a onclick function that will change the state.friends.clicked to true
+  cardClicked = id => {
+    this.shuffleFriends();
+  }
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
@@ -23,6 +51,7 @@ class App extends Component {
         <Title>Clicky Game</Title>
         {this.state.friends.map(friend => (
           <FriendCard
+            cardClicked={this.cardClicked}
             id={friend.id}
             key={friend.id}
             name={friend.name}
--- clicky/src/components/FriendCard/index.js
+++ clicky/src/components/FriendCard/index.js
@@ -4,17 +4,11 @@ import "./style.css";
 // Need to add onClick ability to the card
 function FriendCard(props) {
   return (
-    <div className="card">
+    <div className="card" onClick={() => props.cardClicked(props.id)}>
       <div className="img-container">
-        <img alt={props.name} src={props.image} />
+          <img alt={props.name} src={props.image} />
       </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
-          </li>
-        </ul>
-      </div>
+      
     </div>
   );
 }
