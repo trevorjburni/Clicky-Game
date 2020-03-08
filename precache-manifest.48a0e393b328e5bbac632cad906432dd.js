594f69966ee138f47856a177b42d17da5a4a6a3c
Pilot <trevorjburni@gmail.com>
2020-03-07 18:35:35 -0700

    Updates

---
+++ precache-manifest.48a0e393b328e5bbac632cad906432dd.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1940217e0406461439446de2598ca8ee",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "d10d1f7a9047a2dece47",
+    "url": "/Clicky-Game/static/css/main.8969f1dd.chunk.css"
+  },
+  {
+    "revision": "68ffb976eb6f3a28033c",
+    "url": "/Clicky-Game/static/js/2.304efbbf.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Clicky-Game/static/js/2.304efbbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d10d1f7a9047a2dece47",
+    "url": "/Clicky-Game/static/js/main.e54ab8f2.chunk.js"
+  },
+  {
+    "revision": "b2c2d1e04f6a9aff7245",
+    "url": "/Clicky-Game/static/js/runtime-main.13911d42.js"
+  },
+  {
+    "revision": "86d3bb82f94606ae1da963e8e9a45b2e",
+    "url": "/Clicky-Game/static/media/bowser.86d3bb82.png"
+  },
+  {
+    "revision": "a571efdd045df5b11d093880b84b5768",
+    "url": "/Clicky-Game/static/media/diddy-kong.a571efdd.png"
+  },
+  {
+    "revision": "bb270da24cd7fd0c0d4b7274c49a3234",
+    "url": "/Clicky-Game/static/media/donkey-kong.bb270da2.png"
+  },
+  {
+    "revision": "b7f270fe23c3f5a2541a29136ebf81b0",
+    "url": "/Clicky-Game/static/media/fox-mccloud.b7f270fe.png"
+  },
+  {
+    "revision": "9395295590018da16d27729c64b1ed40",
+    "url": "/Clicky-Game/static/media/kirby.93952955.png"
+  },
+  {
+    "revision": "321ca5556fd0059c274c62469f90027f",
+    "url": "/Clicky-Game/static/media/link.321ca555.png"
+  },
+  {
+    "revision": "7d1565d3fe7aea1a4ef3314544037a07",
+    "url": "/Clicky-Game/static/media/luigi.7d1565d3.png"
+  },
+  {
+    "revision": "c8ae1643ce4c9497978ad219b7e4cd17",
+    "url": "/Clicky-Game/static/media/mario.c8ae1643.png"
+  },
+  {
+    "revision": "109088f02e52d2a1043178f10724b3e0",
+    "url": "/Clicky-Game/static/media/princess-peach.109088f0.png"
+  },
+  {
+    "revision": "a61afa2e1fc5081f98c497b1aea6ee2c",
+    "url": "/Clicky-Game/static/media/super-smash.a61afa2e.png"
+  },
+  {
+    "revision": "a27d595340d93253173c981acd568370",
+    "url": "/Clicky-Game/static/media/toad.a27d5953.png"
+  },
+  {
+    "revision": "5f6bda24706628c572e29a927ffde285",
+    "url": "/Clicky-Game/static/media/wario.5f6bda24.png"
+  },
+  {
+    "revision": "704f6fa2a8a5fb9d719dc12e16c65384",
+    "url": "/Clicky-Game/static/media/yoshi.704f6fa2.png"
+  }
+]);
