fec13cbb34d623c28e6653b026b418d996f6d9a5
Pilot <trevorjburni@gmail.com>
2020-03-07 23:04:16 -0700

    added files from hopefully-working-branch

---
+++ src/components/Nav/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function Nav(props) {
+  return (  
+  <nav className="navbar">
+    <h1 className="title">Super Smash Bros Clicky Game</h1>
+    <span className="navbar-text">Score: {props.score}</span>
+    <span className="navbar-text">Highscore: {props.highscore}</span> 
+    {props.children}
+  </nav>
+  );
+}
+
+export default Nav;
