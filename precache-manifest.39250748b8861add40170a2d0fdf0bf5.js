8765790c7de59852a46ac94233392d9cc7a033b9
Pilot <trevorjburni@gmail.com>
2020-03-07 22:31:19 -0700

    Updates

---
+++ precache-manifest.39250748b8861add40170a2d0fdf0bf5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4d8766d97d2d0aaab6d20825143ee8d",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "ff7c36d49e937b518dc0",
+    "url": "/clicky-game/static/css/main.47e51169.chunk.css"
+  },
+  {
+    "revision": "68ffb976eb6f3a28033c",
+    "url": "/clicky-game/static/js/2.304efbbf.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/clicky-game/static/js/2.304efbbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff7c36d49e937b518dc0",
+    "url": "/clicky-game/static/js/main.e54ab8f2.chunk.js"
+  },
+  {
+    "revision": "3990ae590157ae61a0db",
+    "url": "/clicky-game/static/js/runtime-main.ab6ebab3.js"
+  },
+  {
+    "revision": "86d3bb82f94606ae1da963e8e9a45b2e",
+    "url": "/clicky-game/static/media/bowser.86d3bb82.png"
+  },
+  {
+    "revision": "a571efdd045df5b11d093880b84b5768",
+    "url": "/clicky-game/static/media/diddy-kong.a571efdd.png"
+  },
+  {
+    "revision": "bb270da24cd7fd0c0d4b7274c49a3234",
+    "url": "/clicky-game/static/media/donkey-kong.bb270da2.png"
+  },
+  {
+    "revision": "b7f270fe23c3f5a2541a29136ebf81b0",
+    "url": "/clicky-game/static/media/fox-mccloud.b7f270fe.png"
+  },
+  {
+    "revision": "9395295590018da16d27729c64b1ed40",
+    "url": "/clicky-game/static/media/kirby.93952955.png"
+  },
+  {
+    "revision": "321ca5556fd0059c274c62469f90027f",
+    "url": "/clicky-game/static/media/link.321ca555.png"
+  },
+  {
+    "revision": "7d1565d3fe7aea1a4ef3314544037a07",
+    "url": "/clicky-game/static/media/luigi.7d1565d3.png"
+  },
+  {
+    "revision": "c8ae1643ce4c9497978ad219b7e4cd17",
+    "url": "/clicky-game/static/media/mario.c8ae1643.png"
+  },
+  {
+    "revision": "109088f02e52d2a1043178f10724b3e0",
+    "url": "/clicky-game/static/media/princess-peach.109088f0.png"
+  },
+  {
+    "revision": "a61afa2e1fc5081f98c497b1aea6ee2c",
+    "url": "/clicky-game/static/media/super-smash.a61afa2e.png"
+  },
+  {
+    "revision": "a27d595340d93253173c981acd568370",
+    "url": "/clicky-game/static/media/toad.a27d5953.png"
+  },
+  {
+    "revision": "5f6bda24706628c572e29a927ffde285",
+    "url": "/clicky-game/static/media/wario.5f6bda24.png"
+  },
+  {
+    "revision": "704f6fa2a8a5fb9d719dc12e16c65384",
+    "url": "/clicky-game/static/media/yoshi.704f6fa2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Clicky-Game/precache-manifest.48a0e393b328e5bbac632cad906432dd.js"
+  "/clicky-game/precache-manifest.39250748b8861add40170a2d0fdf0bf5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Clicky-Game/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clicky-game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
